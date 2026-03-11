628d9c45538df79e94bee0d3637ea08e061343e6
joshweakerz <joshuahabana06@gmail.com>
2026-03-11 13:48:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
 
   AOS.init({
     once: false,
     duration: 800,
-    easing: 'ease-in-out'
+    easing: "ease-in-out"
   });
 
   const navbarLinks = document.querySelectorAll('.navbar a');
 
   navbarLinks.forEach(link => {
-    link.addEventListener('click', e => {
+
+    link.addEventListener("click", function (e) {
+
+      const targetId = this.getAttribute("href");
+
+      if (!targetId.startsWith("#")) return;
+
       e.preventDefault();
 
-      const targetId = link.getAttribute('href');
       const section = document.querySelector(targetId);
-      if (!section) return;
 
-      section.scrollIntoView({ behavior: 'smooth' });
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
 
-      const animation = section.getAttribute('data-aos');
-      const delay = section.getAttribute('data-aos-delay');
-      const duration = section.getAttribute('data-aos-duration');
+        setTimeout(() => {
+          AOS.refresh();
+        }, 500);
+      }
 
-      section.removeAttribute('data-aos');
-      void section.offsetWidth;
-
-      section.setAttribute('data-aos', animation);
-      section.setAttribute('data-aos-delay', delay);
-      section.setAttribute('data-aos-duration', duration);
-
-      AOS.refresh();
     });
+
   });
 
-};
+});
